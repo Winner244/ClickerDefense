79d4885aae3ecfb6a9e59c7822be35a9989af948
aleksandr.panfilov <aleksandr.panfilov>
2020-12-06 18:37:43 +0300

    added flyEarch with animation

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ let widthOfCanvas = canvas.width;
 
 //draw grass
 let imageGrass = new Image();   
-imageGrass.onload = drawGrass;;
 imageGrass.src = './media/img/grass1.png';
 
 /** прорисовка травы на веё нижней части canvas */
@@ -17,18 +16,80 @@ function drawGrass(){
 
 //draw fly earch
 let imageFlyEarch = new Image();  
-imageFlyEarch.onload = drawFlyEarch;
 imageFlyEarch.src = './media/img/builders/flyEarth.png';
+let flyEarchWidth = 250;
+let flyEarchHeight = 186;
+let flyEarchFrames = 4;
 
 /** прорисовка анимированной летающей земли в центре экрана */
-function drawFlyEarch(){
+function drawFlyEarch(frame){
 	ctx.drawImage(imageFlyEarch, 
-		0, //crop from x
+		imageFlyEarch.width / flyEarchFrames * frame, //crop from x
 		0, //crop from y
-		imageFlyEarch.width / 4, //crop by width
+		imageFlyEarch.width / flyEarchFrames, //crop by width
 		imageFlyEarch.height,    //crop by height
-		widthOfCanvas / 2 - imageFlyEarch.width / 4 / 2, //draw from x
-		heightOfCanvas / 2 - imageFlyEarch.height / 2,  //draw from y
-		imageFlyEarch.width / 4, //draw by width 
-		imageFlyEarch.height); //draw by height 
+		widthOfCanvas / 2 - flyEarchWidth / 2, //draw from x
+		heightOfCanvas / 2 - flyEarchHeight / 2,  //draw from y
+		flyEarchWidth, //draw by width 
+		flyEarchHeight); //draw by height 
 }
+
+let fps = 0;
+let oldFPStime = '';
+function checkFPS(){
+	let newDate = new Date();
+	let newKey = newDate.getMinutes() + '_' + newDate.getSeconds();
+	if(oldFPStime != newKey){
+		console.log('fps: ' + fps);
+		fps = 0;
+		oldFPStime = newKey;
+	}
+
+	fps++;
+}
+
+/** Жизненный цикл игры */
+let animationId = null;
+let isGameStarted = true;
+function draw(millisecondsFromStart){
+	if(!isGameStarted){
+		return;
+	}
+
+	//проверка что все изображения загружены - иначе будет краш хрома
+	if(!imageGrass.complete || !imageFlyEarch.complete){
+		animationId = window.requestAnimationFrame(draw);
+		return;
+	}
+
+	let partOfSecond = millisecondsFromStart % 1000;
+
+	checkFPS();
+	
+	//очищаем холст
+	ctx.clearRect(0, 0, widthOfCanvas, heightOfCanvas);
+
+	//затемняем фон
+	ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+	ctx.fillRect(0, 0, widthOfCanvas, heightOfCanvas);
+
+	drawGrass(); //травка
+	drawFlyEarch(Math.floor(partOfSecond / (1000 / flyEarchFrames / 2)) % flyEarchFrames); //летающая земля (2 полных цикла анимации за 1 секунду)
+
+	window.requestAnimationFrame(draw);
+}
+animationId = window.requestAnimationFrame(draw);
+
+
+window.addEventListener('keypress', event => {
+	if(event.key == ' '){
+		if(isGameStarted){
+			cancelAnimationFrame(animationId);
+			isGameStarted = false;
+		}
+		else{
+			isGameStarted = true;
+			animationId = window.requestAnimationFrame(draw);
+		}
+	}
+})
