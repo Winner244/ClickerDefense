deb4e6bc400a90515576c2fbb861de4f8bceac7c
aleksandr.panfilov <alexander.panfilov@sharp-dev.net>
2021-10-16 13:13:35 +0300

    updated testing pages

--- src/reactApp/pages/Test/TestPage.tsx
+++ src/reactApp/pages/Test/TestPage.tsx
@@ -11,12 +11,16 @@ import { Waves } from '../../../gameApp/gameSystems/Waves';
 import { Helper } from '../../helpers/Helper';
 import { Draw } from '../../../gameApp/gameSystems/Draw';
 import { MovingObject } from '../../../models/MovingObject';
+import {Gamer} from "../../../gameApp/gameObjects/Gamer";
+import {Zombie} from "../../../gameApp/monsters/Zombie";
+import {WaveData} from "../../../gameApp/gameObjects/WaveData";
+import {Boar} from "../../../gameApp/monsters/Boar";
 
 class TestPage extends React.Component {
     componentDidMount(){
         let variant: any = Helper.getUrlQuery()['variant'];
         switch(+variant){
-            case 1:
+            case 1: //разрушение башни
                 App.Store.dispatch(MenuStore.actionCreators.startGame());
                 Game.startNew();
                 Waves.delayEndTimeLeft = Waves.delayStartTimeLeft = 0;
@@ -24,7 +28,7 @@ class TestPage extends React.Component {
                 Buildings.all[Buildings.all.length - 1].health = 1;
             break;
 
-            default: 
+            case 2: //кабаны
                 App.Store.dispatch(MenuStore.actionCreators.startGame());
                 Game.startNew();
                 Waves.delayEndTimeLeft = Waves.delayStartTimeLeft = 0;
@@ -38,15 +42,31 @@ class TestPage extends React.Component {
                 tower.arrows.push(new MovingObject(600, Draw.canvas.height - Game.bottomShiftBorder - Tower.imageArrow.height - z, Tower.imageArrow.width, Tower.imageArrow.height, 1000 * 60, 0, 0, z2));
                 tower.arrows.push(new MovingObject(700, Draw.canvas.height - Game.bottomShiftBorder - Tower.imageArrow.height - z, Tower.imageArrow.width, Tower.imageArrow.height, 1000 * 60, 0, 0, z2));
 
-                
+
                 tower.arrows.push(new MovingObject(200, Draw.canvas.height - Game.bottomShiftBorder - Tower.imageArrow.height - z, Tower.imageArrow.width, Tower.imageArrow.height, 1000 * 60, 0, 0, 400));
                 tower.arrows.push(new MovingObject(300, Draw.canvas.height - Game.bottomShiftBorder - Tower.imageArrow.height - z, Tower.imageArrow.width, Tower.imageArrow.height, 1000 * 60, 0, 0, 400));
                 tower.arrows.push(new MovingObject(400, Draw.canvas.height - Game.bottomShiftBorder - Tower.imageArrow.height - z, Tower.imageArrow.width, Tower.imageArrow.height, 1000 * 60, 0, 0, 400));
                 tower.arrows.push(new MovingObject(500, Draw.canvas.height - Game.bottomShiftBorder - Tower.imageArrow.height - z, Tower.imageArrow.width, Tower.imageArrow.height, 1000 * 60, 0, 0, 400));
                 tower.arrows.push(new MovingObject(600, Draw.canvas.height - Game.bottomShiftBorder - Tower.imageArrow.height - z, Tower.imageArrow.width, Tower.imageArrow.height, 1000 * 60, 0, 0, 400));
                 tower.arrows.push(new MovingObject(700, Draw.canvas.height - Game.bottomShiftBorder - Tower.imageArrow.height - z, Tower.imageArrow.width, Tower.imageArrow.height, 1000 * 60, 0, 0, 400));
-                Buildings.all.push(tower); 
+                Buildings.all.push(tower);
                 Waves.isStarted = false;
+                break;
+
+            default: //стрелы
+                App.Store.dispatch(MenuStore.actionCreators.startGame());
+                Game.startNew();
+                Waves.delayEndTimeLeft = Waves.delayStartTimeLeft = 0;
+                Waves.all = [ //монстры на волнах
+                    { //1-я волна
+                        [Zombie.name]: new WaveData(Zombie.images[0].height,7, 80, 0),
+                        [Boar.name]: new WaveData(Boar.images[0].height,5, 60, 0)
+                    },
+                    { //2-я волна
+                        [Zombie.name]: new WaveData(Zombie.images[0].height, 33, 10, 0),
+                        [Boar.name]: new WaveData(Boar.images[0].height,15, 10, 0)
+                    }];
+                break;
 
         }
     }
