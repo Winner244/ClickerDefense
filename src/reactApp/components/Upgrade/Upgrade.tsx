09cfef84a46be9ff58fa1f6c853b9fa524dee1d7
aleksandr.panfilov <alexander.panfilov@sharp-dev.net>
2022-05-08 17:40:24 +0300

    upgrade-tower

--- src/reactApp/components/Upgrade/Upgrade.tsx
+++ src/reactApp/components/Upgrade/Upgrade.tsx
@@ -58,6 +58,18 @@ export class Upgrade extends React.Component<Props, {}> {
                 <div className="upgrade__close-body">x</div>
             </div>
             <div className="upgrade__container">
+              <div className="upgrade__main-box">
+                <div className="upgrade__info">
+                  <img className="upgrade__image" src={this.props.selectedBuilding.image.src} />
+                  <ul>
+                    <li className="upgrade__parameter">
+                      <div className="upgrade__parameter-name">Здоровье</div>: <div className="upgrade__parameter-value">{this.props.selectedBuilding.healthMax}</div>
+                    </li>
+                  </ul>
+                </div>
+                <div className="upgrade__upgrade-items"></div>
+              </div>
+              <div className="upgrade__upgraded-box"></div>
             </div>
         </div>
     </div>
