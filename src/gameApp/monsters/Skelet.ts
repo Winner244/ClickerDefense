95531805151b60c1925e688fe55b675b59710cfe
alexander.panfilov <alexander.panfilov@sharp-dev.net>
2023-09-06 11:42:39 +0700

    connected skelet 2 + updated size of images

--- src/gameApp/monsters/Skelet.ts
+++ src/gameApp/monsters/Skelet.ts
@@ -14,10 +14,13 @@ import {AttackedObject} from '../../models/AttackedObject';
 import {WaveData} from '../../models/WaveData';
 
 import Skelet1Image from '../../assets/img/monsters/skelet/skelet.png'; 
+import Skelet2Image from '../../assets/img/monsters/skelet/skelet2.png'; 
 
 import SkeletAttack1Image from '../../assets/img/monsters/skelet/skeletAttack.png'; 
+import SkeletAttack2Image from '../../assets/img/monsters/skelet/skeletAttack2.png'; 
 
 import SkeletCreating1Image from '../../assets/img/monsters/skelet/creating.png'; 
+import SkeletCreating2Image from '../../assets/img/monsters/skelet/creating2.png'; 
 
 import SkeletCreating1Sound from '../../assets/sounds/monsters/skeletes/creating.mp3'; 
 import SkeletCreating2Sound from '../../assets/sounds/monsters/skeletes/creating2.mp3'; 
@@ -83,10 +86,13 @@ export class Skelet extends Monster{
 	static init(isLoadResources: boolean = true): void{
 		if(isLoadResources && !Skelet.images.length){
 			Skelet.imageHandler.add(Skelet.images).src = Skelet1Image;
+			Skelet.imageHandler.add(Skelet.images).src = Skelet2Image;
 			
 			Skelet.imageHandler.add(Skelet.attackImages).src = SkeletAttack1Image;
+			Skelet.imageHandler.add(Skelet.attackImages).src = SkeletAttack2Image;
 
 			Skelet.imageHandler.add(Skelet.creatingImages).src = SkeletCreating1Image;
+			Skelet.imageHandler.add(Skelet.creatingImages).src = SkeletCreating2Image;
 
 			AudioSystem.load(SkeletCreating1Sound);
 			AudioSystem.load(SkeletCreating2Sound);
