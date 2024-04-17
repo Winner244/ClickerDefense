3cbf69270d75aba1e42c00428e5fb950ffcd94ee
alexander.panfilov <alexander.panfilov@sharp-dev.net>
2024-04-17 08:51:49 +0700

    Panel. hover

--- src/reactApp/components/Panels/Panels.tsx
+++ src/reactApp/components/Panels/Panels.tsx
@@ -228,15 +228,17 @@ export class Panels extends React.Component<Props, {}> {
             <div key={index2} 
               onClick={() => this.onClickSelectItem(item?.id)}
               className={className}>
-                {item == null 
-                  ? null 
-                  : <div className={"panel__item-img-gif nodrag "} style={{backgroundImage: `url(${item.imageGif.src})`}} />}
-                {item == null 
-                  ? null 
-                  : <div className={"panel__item-img nodrag "} style={{backgroundImage: `url(${item.image.src})`}} />}
-                {item == null 
-                  ? null 
-                  : <div className="panel__item-number noselect">{index == 1 ? "Alt + " : index == 2 ? "Ctrl + " : ""}{(index2 + 1) % 10}</div>}
+                <div className='panel__item-container'>
+                  {item == null 
+                    ? null 
+                    : <div className={"panel__item-img-gif nodrag "} style={{backgroundImage: `url(${item.imageGif.src})`}} />}
+                  {item == null 
+                    ? null 
+                    : <div className={"panel__item-img nodrag "} style={{backgroundImage: `url(${item.image.src})`}} />}
+                  {item == null 
+                    ? null 
+                    : <div className="panel__item-number noselect">{index == 1 ? "Alt + " : index == 2 ? "Ctrl + " : ""}{(index2 + 1) % 10}</div>}
+                </div>
                 <canvas width="320" height="320" className={`panel__item-canvas panel${index}__item${index2}-canvas`}></canvas>
             </div>
           );
